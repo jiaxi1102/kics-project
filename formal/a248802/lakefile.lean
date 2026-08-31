5e6923b1005095d77010e426a0c5b991730597bc
Jake Zhao <jiaxi_1@hotmail.com>
2026-08-31 14:28:55 -0400

    Mark Lean proof library as default build target

--- formal/a248802/lakefile.lean
+++ formal/a248802/lakefile.lean
@@ -6,5 +6,6 @@ package «a248802_counterexample» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
 
+@[default_target]
 lean_lib A248802Counterexample where
   roots := #[`A248802Counterexample]
